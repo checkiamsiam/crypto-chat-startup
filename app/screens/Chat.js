f49b5680fdcd5de5ff929e4897b89575a8e7f4ad
checkiamsiam <issiam02415@gmail.com>
2023-07-30 16:34:40 +0600

    searchbar added

--- app/screens/Chat.js
+++ app/screens/Chat.js
@@ -1,15 +1,20 @@
-import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
 import {
   KeyboardAvoidingView,
   Platform,
   SafeAreaView,
-  ScrollView,
+  TextInput,
   View,
 } from "react-native";
 import HomeHeader from "../layouts/HomeHeader";
+import { Colors } from "../theme/color";
 import style from "../theme/style";
 
 export default function ChatScreen() {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (text) => {};
   return (
     <SafeAreaView style={[style.area]}>
       <KeyboardAvoidingView
@@ -18,8 +23,36 @@ export default function ChatScreen() {
       >
         <View style={[style.main]}>
           <HomeHeader />
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              backgroundColor: Colors.secondary,
+              height: 48,
+              paddingHorizontal: 12,
+              marginHorizontal: 2,
+              borderRadius: 20,
+            }}
+          >
+            <Ionicons
+              name="ios-search-outline"
+              size={24}
+              color={Colors.disable}
+            />
 
-          <ScrollView showsVerticalScrollIndicator={false} />
+            <TextInput
+              style={{
+                width: "100%",
+                height: "100%",
+                marginHorizontal: 12,
+              }}
+              value={search}
+              onChangeText={handleSearch}
+              placeholder="Search contact..."
+            />
+          </View>
+
+          {/* <ScrollView showsVerticalScrollIndicator={false} /> */}
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
