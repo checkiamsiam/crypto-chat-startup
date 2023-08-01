4dd162fc4827b2fcf3da8cb8b63a45b3cd30f963
checkiamsiam <issiam02415@gmail.com>
2023-08-01 13:37:58 +0600

    chat feature done

--- app/screens/PersonalChat.js
+++ app/screens/PersonalChat.js
@@ -11,16 +11,14 @@ import {
   ScrollView,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
-import EmojiSelector from "react-native-emoji-selector";
-import demoImage from "../assets/image/camera.png";
 import PersonalChatHeader from "../layouts/PersonalChatHeader";
 import { Colors } from "../theme/color";
 import style from "../theme/style";
 
 const PersonalChat = () => {
-  const [showEmojiSelector, setShowEmojiSelector] = useState(false);
   const [selectedMessages, setSelectedMessages] = useState([]);
   const [messages, setMessages] = useState([
     {
@@ -54,7 +52,7 @@ const PersonalChat = () => {
   ]);
   const [recepientData, setRecepientData] = useState();
   const navigation = useNavigation();
-  const [selectedImage, setSelectedImage] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
   const route = useRoute();
   const { recepientId } = route.params;
   const [message, setMessage] = useState("");
@@ -75,10 +73,6 @@ const PersonalChat = () => {
     scrollToBottom();
   };
 
-  const handleEmojiPress = () => {
-    setShowEmojiSelector(!showEmojiSelector);
-  };
-
   const handleSend = async (messageType) => {
     if (messageType === "text") {
       setMessages([
@@ -93,19 +87,34 @@ const PersonalChat = () => {
       ]);
       setMessage("");
     }
+    if (messageType === "image") {
+      if (selectedImage) {
+        setMessages([
+          ...messages,
+          {
+            id: 1,
+            imgSrc: selectedImage,
+            messageType: "image",
+            senderId: 1,
+            timeStamp: new Date(),
+          },
+        ]);
+        setSelectedImage(null);
+      }
+    }
   };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
     });
 
-    console.log(result);
     if (!result.canceled) {
-      handleSend("image", result.uri);
+      setSelectedImage(result.assets[0].uri);
+      // result.assets.map((item) => {
+      //   handleSend("image", item.uri);
+      //   console.log(item.uri);
+      // });
     }
   };
 
@@ -191,7 +200,7 @@ const PersonalChat = () => {
                   <Pressable
                     key={index}
                     style={[
-                      item?.senderId?._id === 1
+                      item?.senderId === 1
                         ? {
                             alignSelf: "flex-end",
                             backgroundColor: Colors.primary,
@@ -212,7 +221,7 @@ const PersonalChat = () => {
                   >
                     <View>
                       <Image
-                        source={demoImage}
+                        source={{ uri: item.imgSrc }}
                         style={{ width: 200, height: 200, borderRadius: 7 }}
                       />
                       <Text
@@ -233,6 +242,22 @@ const PersonalChat = () => {
               }
             })}
           </ScrollView>
+          {selectedImage && (
+            <View
+              style={{ padding: 20, backgroundColor: "grey", borderRadius: 10 }}
+            >
+              <TouchableOpacity
+                onPress={() => setSelectedImage(null)}
+                style={{ alignSelf: "flex-end", marginRight: 5 }}
+              >
+                <Entypo name="cross" size={24} />
+              </TouchableOpacity>
+              <Image
+                source={selectedImage && { uri: selectedImage }}
+                style={{ width: 200, height: 200, borderRadius: 7 }}
+              />
+            </View>
+          )}
           <View
             style={{
               flexDirection: "row",
@@ -244,9 +269,9 @@ const PersonalChat = () => {
             }}
           >
             <Entypo
-              onPress={handleEmojiPress}
+              onPress={pickImage}
+              name="camera"
               style={{ marginRight: 5 }}
-              name="emoji-happy"
               size={24}
               color="gray"
             />
@@ -263,7 +288,6 @@ const PersonalChat = () => {
                 paddingHorizontal: 10,
               }}
               placeholder="Type Your message..."
-              onFocus={() => setShowEmojiSelector(false)}
             />
 
             <View
@@ -274,45 +298,36 @@ const PersonalChat = () => {
                 marginHorizontal: 8,
               }}
             >
-              <Entypo
-                onPress={pickImage}
-                name="camera"
-                size={24}
-                color="gray"
-              />
-
+              <Entypo name="attachment" size={24} color="gray" />
               <Feather name="mic" size={24} color="gray" />
             </View>
 
-            <Pressable onPress={() => handleSend("text")}>
+            <View>
               {message.length > 0 ? (
-                <Ionicons name="send" size={24} color={Colors.primary} />
+                <Ionicons
+                  onPress={() => handleSend("text")}
+                  name="send"
+                  size={24}
+                  color={Colors.primary}
+                />
               ) : (
-                <Text
-                  style={{
-                    color: "white",
-                    fontWeight: "bold",
-                    backgroundColor: Colors.primary,
-                    paddingVertical: 8,
-                    paddingHorizontal: 12,
-                    borderRadius: 20,
-                  }}
-                >
-                  Send
-                </Text>
+                <Pressable onPress={() => handleSend("image")}>
+                  <Text
+                    style={{
+                      color: "white",
+                      fontWeight: "bold",
+                      backgroundColor: Colors.primary,
+                      paddingVertical: 8,
+                      paddingHorizontal: 12,
+                      borderRadius: 20,
+                    }}
+                  >
+                    Send
+                  </Text>
+                </Pressable>
               )}
-            </Pressable>
+            </View>
           </View>
-
-          {showEmojiSelector && (
-            <EmojiSelector
-              showSearchBar={false}
-              onEmojiSelected={(emoji) => {
-                setMessage((prevMessage) => prevMessage + emoji);
-              }}
-              style={{ height: 250 }}
-            />
-          )}
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
